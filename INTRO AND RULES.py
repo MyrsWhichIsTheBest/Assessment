96f6dbcbec8e6927a289d4ccd71201b79aa8dd0c
Tim <nat@middleton.school.nz>
2022-05-05 09:20:53 +1200

    something

--- INTRO AND RULES.py
+++ INTRO AND RULES.py
@@ -5,16 +5,21 @@ def strip_string(string):
     return "".join(string.rstrip().lstrip())
 
 
-def yes_no(answer):
+def yes_no(answer, loop):
     strip_string(answer)
-    if answer == "yes":
-        return True
-    elif answer == "no":
-        return False
+    if answer == "yes" or answer == "y":
+        loop = True
+        return True, loop
+    elif answer == "no" or answer == "n":
+        loop
+        return False, loop
     else:
-        print("Un ")
+        print("Please put in Yes or No")
+        return
 
 
+loop_check = False
+instructions = ""
 print("Welcome to the Te Reo Counting Quiz!\n"
       "*clapping sounds*\n"
       "Now time to introduce the contestant for tonight!")
@@ -22,9 +27,10 @@ time.sleep(0.4)
 name = input("Whats your name?: ")
 time.sleep(0.4)
 print(f"Welcome, {name}, to the Show!\n")
-time.sleep(0.8)
-instructions = input("Do you want to read the rules? Its not (too) hard! (Y/N)").lower()
-
+while not loop_check:
+    time.sleep(0.8)
+    instructions = input("Do you want to read the rules? Its not (too) hard! (Y/N)").lower()
+    yes_no(instructions, loop_check)
 if instructions:
     time.sleep(1)
     print("OK! I hope you brushed up on your Te Reo skills, this will be a Quiz!\n"
